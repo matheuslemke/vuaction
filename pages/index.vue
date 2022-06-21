046f03902f426a72c28c5a14dfa121c8d82c4971
Anderson Lemke <lemke@taller.net.br>
2022-06-21 09:40:44 -0300

    feat: add main structure for index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>Vueaction</h1>
+    <div v-if="!testStarted">
+      <h4>Test your reaction time</h4>
+      <button @click="testStarted = true">Start test</button>
+    </div>
+    <div v-if="testStarted">Wait for green</div>
+    <div>Click it!!!</div>
+    <div>
+      Results here
+      <button>Next try</button>
+      <button>Restart</button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,5 +20,10 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  data() {
+    return {
+      testStarted: false,
+    }
+  },
 })
 </script>
