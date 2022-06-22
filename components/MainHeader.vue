5d03e23f35221448966e9964d2adeb6a92ebd7bf
Anderson Lemke <lemke@taller.net.br>
2022-06-22 18:08:05 -0300

    feat: start reaction test an wait for screen

---
+++ components/MainHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="header" class="bg-gradient-to-b bg-gray-600 pb-6">
+    <div
+      class="min-w-full text-center text-gray-200 text-4xl font-bold tracking-widest leading-loose"
+    >
+      Vueaction
+    </div>
+    <div class="min-w-full text-center text-gray-400 font-bold tracking-wide">
+      Test your reaction time
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    <h1>Vueaction</h1>
-    <div v-if="!testStarted">
-      <h4>Test your reaction time</h4>
-      <button @click="testStarted = true">Start test</button>
+  <div class="min-h-screen flex flex-col">
+    <MainHeader />
+    <div
+      v-if="!testStarted"
+      class="bg-gray-300 flex text-center items-center place-content-center centered"
+    >
+      <button
+        class="bg-green-700 hover:bg-green-900 text-gray-100 py-2 px-4 rounded shadow"
+        @click="testStarted = true"
+      >
+        Start test
+      </button>
     </div>
-    <div v-if="testStarted">Wait for green</div>
-    <div>Click it!!!</div>
-    <div>
+    <div v-if="testStarted" class="cursor-pointer" @click="guessClick">
+      <div
+        class="bg-red-500 flex text-center text-3xl text-gray-100 tracking-widest leading-loose items-center place-content-center centered"
+      >
+        Wait for green
+      </div>
+    </div>
+    <div v-if="shouldShowResults">
       Results here
       <button>Next try</button>
       <button>Restart</button>
@@ -17,13 +29,27 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import MainHeader from '../components/MainHeader.vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  components: { MainHeader },
   data() {
     return {
       testStarted: false,
+      shouldShowResults: false,
     }
+  },
+  methods: {
+    guessClick: () => {
+      console.log('asdada')
+    },
   },
 })
 </script>
+
+<style>
+.centered {
+  min-height: calc(100vh - 120px);
+}
+</style>
